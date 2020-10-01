29c99b06cee7317160637483729f70a0ea5367f8
Mario Fronza <mariofronza1@gmail.com>
2020-09-30 22:18:46 -0300

    feat: added api exception class and constants

--- src/main/java/com/github/transactionmock/exceptions/ApiException.java
+++ src/main/java/com/github/transactionmock/exceptions/ApiException.java
@@ -2,10 +2,15 @@ package com.github.transactionmock.exceptions;
 
 import org.springframework.http.HttpStatus;
 
-public class ApiException {
+public class ApiException extends Exception {
 
     private final String message;
     private final HttpStatus httpStatus;
+
+    public ApiException(String message) {
+        this.message = message;
+        this.httpStatus = HttpStatus.BAD_REQUEST;
+    }
 
     public ApiException(String message, HttpStatus httpStatus) {
         this.message = message;
---
+++ src/main/java/com/github/transactionmock/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.github.transactionmock.utils;
+
+public class Constants {
+
+    public static final int MIN_ID = 1000;
+    public static final int MAX_ID = 100000000;
+    public static final int MIN_MONTH = 1;
+    public static final int MAX_MONTH = 12;
+    public static final int MIN_DESCRIPTION = 10;
+    public static final int MAX_DESCRIPTION = 60;
+    public static final int MIN_VALUE = -9999999;
+    public static final int MAX_VALUE = 9999999;
+
+}
